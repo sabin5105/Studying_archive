90a88f3e8afcce023fe214c4d66c5ad666ca14e3
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-04-11 17:48:44 +0900

    04.11

---
+++ 공부/python_advanced/myvenv/Chapter04/05.inheritance.py
@@ -0,0 +1,44 @@
+class Item:
+    """
+    속성 : 이름
+    메서드 : 줍기, 버리기
+    """
+
+    def __init__(self, name):
+        self.name = name
+    
+    def pick(self):
+        print(f"[{self.name}]을(를) 주웠습니다!")
+    
+    def discard(self):
+        print(f"[{self.name}]을(를) 버렸습니다!")
+
+class weapon(Item):
+    """
+    속성 : 공격력
+    메서드 : 공격하기
+    """
+    def __init__(self, name, damage):
+        super().__init__(name)
+        self.damage = damage
+    
+    def attack(self):
+        print(f"[{self.name}]을(를) 이용해 {self.damage}로 공격합니다!")
+    
+class HealingItem(Item):
+    """
+    속성 : 회복량
+    메서드 : 사용하기
+    """
+    def __init__(self, name, recovery_amount):
+        super().__init__(name)
+        self.recovery_amount = recovery_amount
+
+    def use(self):
+        print(f"[{self.name}]을(를) 사용해 {self.recovery_amount}만큼 회복합니다")
+
+m16 = weapon("m16",110)
+bandid = HealingItem("붕대",20)
+
+m16.attack()
+bandid.use()
---
+++ 공부/python_advanced/myvenv/Chapter04/07.class_prob.py
@@ -0,0 +1,64 @@
+
+unit_info = {
+    "probe": {
+        "name" : "프로브",
+        "mineral" : 50,
+        "gas" : 0,
+        "hp" : 20,
+        "shield" : 20,
+        "demage" : 5
+    },
+    "zealot": {
+        "name" : "질럿",
+        "mineral" : 50,
+        "gas" : 0,
+        "hp" : 20,
+        "shield" : 20,
+        "demage" : 5
+    },
+    "dragon": {
+        "name" : "드라군",
+        "mineral" : 50,
+        "gas" : 100,
+        "hp" : 20,
+        "shield" : 20,
+        "demage" : 5
+    }
+}
+
+class Unit:
+    def __init__(self, name, mineral, gas, hp, shield, demage):
+        self.name = name
+        self.mineral = mineral
+        self.gas = gas
+        self.hp = hp
+        self.shield = shield
+        self.demage = demage
+    
+        
+
+class Player:
+    
+    def __init__(self, nickname, mineral, gas, unitlist = []):
+        self.nickname = nickname
+        self.mineral = mineral
+        self.gas = gas
+        self.unitlist = unitlist
+
+    def produce(self, name, mineral, gas, hp, shield, demage):
+        if self.mineral < mineral:
+           print("미네랄이 부족합니다")
+        elif self.gas < gas:
+            print("가스가 부족합니다")
+        else:
+            self.mineral -= mineral
+            self.gas -= gas
+            unit = Unit(name,mineral,gas,hp,shield,demage)
+            self.unitlist.append(unit)
+            print(f"[{name}]을 생성합니다")
+
+player1 = Player('bisu',400,10)
+
+player1.produce(unit_info['probe']['name'], unit_info['probe']['mineral'],unit_info['probe']['gas'],unit_info['probe']['hp'],unit_info['probe']['shield'],unit_info['probe']['demage'])
+player1.produce(unit_info['zealot']['name'], unit_info['zealot']['mineral'],unit_info['zealot']['gas'],unit_info['zealot']['hp'],unit_info['zealot']['shield'],unit_info['zealot']['demage'])
+player1.produce(unit_info['dragon']['name'], unit_info['dragon']['mineral'],unit_info['dragon']['gas'],unit_info['dragon']['hp'],unit_info['dragon']['shield'],unit_info['dragon']['demage'])
