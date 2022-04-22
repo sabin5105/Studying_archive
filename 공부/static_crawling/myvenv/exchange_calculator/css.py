8e938db8efd89a84f1affd73c3a84cd39449b672
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-04-22 16:32:07 +0900

    04.22 study

---
+++ 공부/static_crawling/myvenv/exchange_calculator/css.py
@@ -0,0 +1,20 @@
+from pydoc import stripid
+from bs4 import BeautifulSoup as BS
+import requests as req
+
+url = 'https://finance.naver.com/marketindex/exchangeList.naver'
+res = req.get(url)
+soup = BS(res.text,"html.parser")
+
+tds = soup.find_all("td")
+
+names = []
+for td in soup.select('td.tit'):
+    names.append(td.get_text(strip=True))
+
+prices = []
+for td in soup.select('td.sale'):
+    prices.append(td.get_text(strip=True))
+
+print(names)
+print(prices)
