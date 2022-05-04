a4a3b83546949fcf4ae96551ed54ba3d4a859f06
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-05-04 18:20:18 +0900

    Delete 공부/python_advanced/myvenv directory

--- 공부/python_advanced/myvenv/Chapter02/04.list_connotation_prob.py
+++
@@ -1,7 +0,0 @@
-word_list = ['apple', 'watch', 'apolo', 'start', 'abocado']
-begins_with_a = [word for word in word_list if word[0] == 'a']
-print(begins_with_a)
-
-mylist = ['omega3', '', 'vitamin C500', '', 'ginger']
-mylist = [word for word in mylist if word != '']
-print(mylist)
