fd1ccda95df848c459b1b6e09ccf01d00748ae29
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-09-19 16:00:12 +0900

    Update object.js
    initialize objects with new / literal

--- Learned/JavaScript/modern/core/object.js
+++ Learned/JavaScript/modern/core/object.js
@@ -1,6 +1,16 @@
-let user = {     // 객체
-    name: "John",  // 키: "name",  값: "John"
-    age: 30        // 키: "age", 값: 30
-  };
-
 // access with dot notation or bracket notation
+
+function User(name) {
+  // this = {};  (빈 객체가 암시적으로 만들어짐)
+
+  // 새로운 프로퍼티를 this에 추가함
+  this.name = name;
+  this.isAdmin = false;
+
+  // return this;  (this가 암시적으로 반환됨)
+}
+
+let user = new User("보라");
+
+console.log(user.name); // 보라
+console.log(user.isAdmin); // false
