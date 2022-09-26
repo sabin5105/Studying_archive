79245cac37993195b887c6ca7fa46da6620ee12f
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-09-26 12:49:22 +0900

    Update email.js to add comment

--- Learned/JavaScript/nodejs/email.js
+++ Learned/JavaScript/nodejs/email.js
@@ -1,5 +1,6 @@
 var nodemailer = require('nodemailer');
 
+// create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -8,6 +9,7 @@ var transporter = nodemailer.createTransport({
   }
 });
 
+// setup email data with unicode symbols
 var mailOptions = {
   from: 'youremail@gmail.com',
   to: 'myfriend@yahoo.com',
@@ -15,6 +17,7 @@ var mailOptions = {
   text: 'That was easy!'
 };
 
+// send mail with defined transport object
 transporter.sendMail(mailOptions, function(error, info){
   if (error) {
     console.log(error);
@@ -22,3 +25,12 @@ transporter.sendMail(mailOptions, function(error, info){
     console.log('Email sent: ' + info.response);
   }
 });
+
+/*
+  the script is for sending email using nodejs
+
+  1. install nodemailer using npm
+  2. create a gmail account
+  3. enable less secure apps in gmail account
+  4. run the script
+*/
